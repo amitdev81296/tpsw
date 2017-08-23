96b34e29bd6911fe31cc6da037bb11a8e01080ba
Amit Nijsure <amitnijsure1234@gmail.com>
2017-08-23 22:41:53 +0530

    Login, Base and Index HTML files created and CSS and JS files added

--- tp/views.py
+++ tp/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'tp/index.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        
+    return render(request, 'tp/login.html')
+
