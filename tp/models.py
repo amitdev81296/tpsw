706399fd847cbc0ec4714b09a1d21711f2b65b4b
Amit Nijsure <amitnijsure1234@gmail.com>
2017-08-26 12:26:04 +0530

    UserProfile Model Finalized and Displayed in Template

--- tp/models.py
+++ tp/models.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django import forms
 from django.utils import timezone
+from datetime import date
 
 
 class UserLoginForm(ModelForm):
@@ -18,15 +19,21 @@ class UserProfile(models.Model):
         ('Female', 'Female'),
         ('Others', 'Others'),
     )
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, unique=True)
     student_id = models.CharField(max_length=10)
     roll_no = models.IntegerField()
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    resume_pdf = models.FileField(upload_to='./resumes_folder', max_length=100)
-    marksheet_pdf = models.FileField(upload_to='./marksheets_folder', max_length=100)
-    document_pdf = models.FileField(upload_to='./documents_folder', max_length=100)
     birth_date = models.DateField(default=timezone.now)
+    profile_photo = models.FileField(upload_to='C:/Users/nijsu/PycharmProjects/tpsw/tp/photos_folder', max_length=100)
+    resume_pdf = models.FileField(upload_to='C:/Users/nijsu/PycharmProjects/tpsw/tp/resumes_folder', max_length=100)
+    marksheet_pdf = models.FileField(upload_to='C:/Users/nijsu/PycharmProjects/tpsw/tp/marksheets_folder', max_length=100)
+    document_pdf = models.FileField(upload_to='C:/Users/nijsu/PycharmProjects/tpsw/tp/documents_folder', max_length=100)
     do_you_have_plans_for_higher_studies = models.BooleanField()
     where_do_you_see_yourself_in_the_coming_two_years = models.TextField(max_length=300)
     why_did_you_choose_engineering = models.TextField(max_length=300)
     allowed = models.BooleanField()
+
+    def get_age(self):
+        today = date.today()
+        dob = self.birth_date
+        return int(today.year) - int(dob.year)
--- tp/views.py
+++ tp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import UserLoginForm
+from .models import UserLoginForm, UserProfile
 from django.contrib.auth import authenticate
 # Create your views here.
 
@@ -20,8 +20,9 @@ def dashboard(request):
         un = request.POST['username']
         pw = request.POST['password']
         user = authenticate(username=un, password=pw)
+        user_profile = UserProfile.objects.get(user = user)
         if user:
-            return render(request, 'tp/dashboard.html', {'user': user})
+            return render(request, 'tp/dashboard.html', {'user': user, 'user_profile': user_profile})
         else:
             return render(request, 'tp/login.html', {'form': empty_form})
 
